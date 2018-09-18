4e4dfa37e54f27246903989681c6c2d9c2e2d6b9
AswathyMA <ma.aswaty@gmail.com>
2018-09-18 18:19:17 +0530

    created basic template for multi-auth in laravel 5.7

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layouts.app');
+});
+Route::prefix('admin')->group(function() {
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/dashboard', 'AdminController@index')->name('admin.dashboard');
+    Route::post('/logout', 'AdminController@logout')->name('admin.logout.submit');
+});
+Auth::routes();
+Route::get('/dashboard', 'HomeController@index')->name('dashboard');
